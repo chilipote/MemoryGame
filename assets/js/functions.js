7e3a224e300f55210acd47f0f2a7000e29a1791e
Allan Bettarel <allan.bettarel@gmail.com>
2013-04-10 19:24:01 -0400

    AttemptCount work, fix bugs

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,7 +24,11 @@ function showNbOfPairs(nbOfPairs) {
 }
 
 function updateScoreCountView(score) {
-	$(".score").html(score);
+	$("#score").html(score);
+}
+
+function updateAttemptCountView(attempt) {
+	$("#attempt").html(attempt);
 }
 
 function generationGame() {
@@ -43,6 +47,99 @@ function removeTheClass() {
 		FirstOfPair.children().hide();
 	});
 	SecondOfPair.children().fadeOut('slow');
+}
+
+
+
+function main() {
+	
+	showModal();	
+
+// CLICK ON VALID LEVEL
+	$(".level-valid").click(function(){
+
+		nbOfRow = $('input[name=nbOfRow]:checked').val();
+		
+		if(!levelValueExist(nbOfRow)) {
+			displayError();
+			return false;
+		}
+
+		var nbPairMemoryItem = nbOfRow * _nbItemOnLine / 2;
+		showNbOfPairs(nbPairMemoryItem);
+		var array1 = new Array();
+		var array2 = new Array();
+
+		_setOfMemory.sort(function() { return 0.5 - Math.random() });
+
+		var setOfMemoryForGame = _setOfMemory.splice(0,nbPairMemoryItem);
+
+		for (var a = setOfMemoryForGame.length; a > 0; a-- ) {
+			array1.push(setOfMemoryForGame[a-1]);
+			array2.push(setOfMemoryForGame[a-1]);
+		}	 				
+		array2.sort(function() { return 0.5 - Math.random() });
+
+		setOfMemoryReadyToUse = array1.concat(array2);
+
+			z = 0;
+
+			generationGame();
+
+			$("#levelModal").modal('hide');
+	});
+
+	// CLOSE POP UP
+	$("#levelModal").on("hide",function(event) {
+		var nbOfRow = $('input[name=nbOfRow]:checked').val();
+				 		
+		if(!levelValueExist(nbOfRow)) {
+			displayError();
+
+			// PAS DECOUPER EN FONCTION SINON LE RETURN FALSE N'EST PAS INTERPRETE
+			return false;
+		}
+	});
+
+	// WAIT FOR EVENT 
+$(document).on("click",".item", function() {
+
+	if (nbClick == 0 ) {
+		
+		FirstOfPair = $(this);
+		FirstOfPair.children().show('slow');
+		
+		nbClick++;
+		return false;
+	}
+
+		if (nbClick == 1) {
+			attempt++;
+			updateAttemptCountView(attempt);
+			$(this).children().show('slow');
+			
+			SecondOfPair = $(this);
+
+			if((FirstOfPair.data('id') != SecondOfPair.data('id')) && (FirstOfPair.data('answer') == SecondOfPair.data('answer'))) {
+				
+				score++;
+				updateScoreCountView(score);
+
+				window.setTimeout(removeTheClass , 1000);
+				
+				$(".item"+FirstOfPair.data('answer')).fadeOut('slow', function() {
+					$(".item"+FirstOfPair.data('answer')).addClass('item-found');
+				});
+				
+				cleanVariable();
+
+			} else {
+
+				window.setTimeout(removeTheClass , 1000);
+				cleanVariable();
+			}
+		}
+	});	
 }
 
 	
