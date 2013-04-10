4628dc2a91ab90148b0d600f9e527de7d92b4d57
Allan Bettarel <allan.bettarel@gmail.com>
2013-04-10 19:00:52 -0400

    [Memory Game] -

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,17 +14,35 @@ function displayError() {
 	$(".modal-error").html("<p>Choisissez d'abord un niveau de difficulté</p>");
 }
 
-function cleanVariable () {
+function cleanVariable() {
 	delete FirstOfPair, SecondOfPair;
 	nbClick = 0;
 }
 
+function showNbOfPairs(nbOfPairs) {
+	$(".nb-pair-memory-item").html(nbOfPairs);
+}
+
 function updateScoreCountView(score) {
-	$("#score").html(score);
+	$(".score").html(score);
+}
+
+function generationGame() {
+	for (var i = nbOfRow; i > 0; i--) {
+		$("#game").prepend("<div id="+i+" class=\"row-fluid\"></div>");
+		for (var j = _nbItemOnLine; j > 0; j--) {
+			setOfMemoryReadyToUse[z]["question"].toString();
+			$("#"+i).append("<div data-id="+z+" data-answer="+setOfMemoryReadyToUse[z]["id"]+" class=\"item item"+setOfMemoryReadyToUse[z]["id"]+" span"+_spanItem+"\"><div class=\"answer\"><p>#"+setOfMemoryReadyToUse[z]["question"].toString()+"</p></div></div>");
+			z++;
+		};
+	};
 }
 
 function removeTheClass() {
-	FirstOfPair.children().removeClass('active');
-	SecondOfPair.children().removeClass('active');
+	FirstOfPair.children().fadeOut('slow', function(){
+		FirstOfPair.children().hide();
+	});
+	SecondOfPair.children().fadeOut('slow');
 }
 
+	
